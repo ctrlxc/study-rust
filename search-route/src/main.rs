59625dcd470b4a6d47887301f7e9ca4640e4918a
Tatsuhide Nakamura <ctrlpfnb@gmail.com>
2020-02-12 00:07:01 +0900

    所有権やライフタイムがまだわかってない...

--- search-route/src/main.rs
+++ search-route/src/main.rs
@@ -1,232 +1,229 @@
 //search-route.rs
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 
 #[derive(Debug, Clone, Copy)]
 struct Pos {
-  x: usize,
-  y: usize,
+    x: usize,
+    y: usize,
 }
 
-fn in_pos(x: usize, y: usize, arr: &Vec<Pos>) -> bool {
-  for a in arr {
-    if a.x == x && a.y == y {
-       return true;
+impl PartialEq for Pos {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
     }
-  }
+}
 
-  return false;
+impl Eq for Pos {}
+
+impl Hash for Pos {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+    }
+}
+
+struct Node {
+    pos: Pos,
+    step: i32,
+    prev: Option<Box<Node>>,
 }
 
 struct GameMap {
-   w: usize,
-   h: usize,
-   rmap: Vec<Vec<String>>,
-   start: Pos,
-   goal: Pos,
-   oks: Vec<Pos>,
-   ngs: Vec<Pos>,
+    w     : usize,
+    h     : usize,
+    hmap  : HashMap<Pos, String>,
+    start : Pos,
+    goal  : Pos,
+    nodes : HashMap<Pos, Node>,
 }
 
 impl GameMap {
-  fn is_valid(&self, x: usize, y: usize, cur_route: &Vec<Pos>) -> bool {
-    if x >= self.w || y >= self.h  {
-       return false;
+    fn new(s: &str) -> GameMap {
+        let mut lines = s.lines();
+
+        let wh: Vec<String> = lines.next().unwrap().split_whitespace()
+            .map(|e| e.parse().ok().unwrap()).collect();
+
+        let rmap: Vec<Vec<String>> = lines.map(|l| l.split_whitespace()
+                                               .map(|e| e.parse().ok().unwrap()).collect()).collect();
+
+        let w: usize = wh[0].parse::<usize>().unwrap();
+        let h: usize = wh[1].parse::<usize>().unwrap();
+
+        let mut start = Pos {x: 0, y: 0};
+        let mut goal = Pos {x: 0, y: 0};
+        let mut hmap: HashMap<Pos, String> = HashMap::new();
+        for (i, l) in rmap.iter().enumerate() {
+            for (j, c) in l.iter().enumerate() {
+                let pos = Pos {x: j, y: i};
+
+                hmap.insert(pos, c.to_string()); //@@@ to_string() naze iru?
+
+                if c == "s" {
+                    start = pos;
+                }
+                else if c == "g" {
+                    goal = pos;
+                }
+            }
+        }
+
+        GameMap {
+            w,
+            h,
+            hmap,
+            start,
+            goal,
+            nodes: HashMap::new(),
+        }
     }
-  
-    if in_pos(x, y, &cur_route)  {
-       return false;
-    }
-  
-    if in_pos(x, y, &self.ngs)  {
-       return false;
-    }
-  
-    true
-  }
 
-  fn is_goal(&self, x: usize, y: usize) -> bool {
-    if x == self.goal.x && y == self.goal.y {
-      return true;
+    fn goal_node(&mut self) -> Option<&Node> {
+        if self.nodes.len() == 0 {
+//@@@            let start_node = self.nodes.insert(self.start, Node {pos: self.start, step: 0, prev: None}).unwrap();
+//@@@            self.parse_nodes(&start_node);
+        }
+
+        self.nodes.get(&self.goal)
     }
 
-    false
-  }
-  
-  fn is_goal_pos(&self, pos: &Pos) -> bool {
-    self.is_goal(pos.x, pos.y)
-  }
+    fn parse_nodes(&mut self, cur_node: &Node) {
+        let next_poses = self.next_poses(&cur_node);
 
-  fn nexts(&self, cur_route: &Vec<Pos>) -> Vec<Pos> {
-    let cur_pos = cur_route.last().unwrap();
+        for next_pos in next_poses {
+            let next_node = self.nodes.entry(next_pos).or_insert(Node {pos: next_pos, step: -1, prev: None});
+            
+            if (*next_node).step < 0 || (*next_node).step > cur_node.step + 1 {
+                (*next_node).step = cur_node.step + 1;
+                //next_node.prev = Some(cur_node); @@@ ???
+            }
 
-    let x = cur_pos.x;
-    let y = cur_pos.y;
-  
-    let pat: [[i32; 2]; 4] = [[0,1],[0,-1],[1,0],[-1,0]];
-  
-    let mut nexts: Vec<Pos> = Vec::new();
-  
-    for p in pat.iter() {
-      if (x == 0 && p[0] < 0) || (y == 0 && p[1] < 0) {
-        continue;
-      }
-
-      let x2 = (x as i32) + p[0];
-      let y2 = (y as i32) + p[1];
-
-      let x2 = x2 as usize;
-      let y2 = y2 as usize;
-
-      if self.is_goal(x2, y2) {
-        return vec![self.goal.clone()];
-      }
-
-      if self.is_valid(x2, y2, &cur_route) {
-        nexts.push(Pos {x: x2, y: y2});
-      }
+            self.parse_nodes(next_node);
+        }
     }
-  
-    nexts
-  }
 
-  fn find_routes(&self, cur_route: &Vec<Pos>, goals: &mut Vec<Vec<Pos>>) {
-    let nexts = self.nexts(cur_route);
-  
-    if nexts.len() == 1 && self.is_goal_pos(&nexts[0]) {
-      goals.push(cur_route.clone());
-      return;
-    }
-  
-    for n in nexts {
-      let mut new_route = cur_route.clone();
-      new_route.push(n);
-      self.find_routes(&new_route, goals);
-    }
-  }
-}
-
-fn init() -> (Vec<String>, Vec<Vec<String>>) {
-   let wh = read_vec::<String>();
-   let w = &wh[0];
-   let h = &wh[1];
-   let rmap = read_vec2::<String>(h.parse::<u32>().unwrap());
-
-   (wh, rmap)
-}
-
-fn print_route(route: &Vec<Pos>) {
-  for (i, r) in route.iter().enumerate() {
-    print!("[{}]({}, {}) -> ", i, r.x, r.y);
-  }
-
-  println!("g");
-}
-
-fn print_route_all(routes: &Vec<Vec<Pos>>) {
-  for (i, r) in routes.iter().enumerate() {
-    print!("[{}] :: ", i);
-    print_route(&r);
-  }
-}
-
-fn find_routes(wh: Vec<String>, rmap: Vec<Vec<String>>) -> Vec<Vec<Pos>> {
-   let w = &wh[0];
-   let h = &wh[1];
-
-   let mut gmap: GameMap = GameMap {
-      w: w.parse::<usize>().unwrap(),
-      h: h.parse::<usize>().unwrap(),
-      rmap,
-      start: Pos {x: 0, y: 0},
-      goal: Pos {x: 0, y: 0},
-      oks: Vec::<Pos>::new(),
-      ngs: Vec::<Pos>::new(),
-   };
-
-   for (i, l) in gmap.rmap.iter().enumerate() {
-      for (j, c) in l.iter().enumerate() {
-        let pos = Pos {x: j, y: i};
+    fn next_poses(&mut self, cur_node: &Node) -> Vec<Pos> {
+        let mut nexts: Vec<Pos> = Vec::new();
         
-        if c == "s" {
-          gmap.start = pos;
-        }
-        else if c == "g" {
-          gmap.goal = pos;
-        }
-        else if c == "1" {
-          gmap.ngs.push(pos);
-        }
-        else if c == "0" {
-          gmap.oks.push(pos);
-        }
-      }
-   }
+        for p in [[0,1],[0,-1],[1,0],[-1,0]].iter() {
+            if (cur_node.pos.x == 0 && p[0] < 0) || (cur_node.pos.y == 0 && p[1] < 0) {
+                continue;
+            }
 
-   let cur_route: Vec<Pos> = vec![gmap.start];
-   let mut goals: Vec<Vec<Pos>> = vec![];
-   
-   gmap.find_routes(&cur_route, &mut goals);
+            let np = Pos {
+                x: ((cur_node.pos.x as i32) + p[0]) as usize,
+                y: ((cur_node.pos.y as i32) + p[1]) as usize,
+            };
 
-   goals
-}
-
-fn min_steps(routes: &Vec<Vec<Pos>>) -> usize {
-  let mut min: usize = 0; 
-  for r in routes.iter() {
-    if min == 0 || r.len() < min {
-      min = r.len();
+            if self.is_valid(&np, &cur_node) {
+                nexts.push(np);
+            }
+        }
+        
+        nexts
     }
-  }
 
-  min
+
+//    fn parse_nodes(&mut self, cur_node: &Node) {
+//        let next_nodes_ = self.next_nodes(&cur_node);
+//
+//        for next_node in next_nodes_ {            
+//            if (*next_node).step < 0 || (*next_node).step > cur_node.step + 1 {
+//                (*next_node).step = cur_node.step + 1;
+//                //next_node.prev = Some(cur_node); @@@ ???
+//            }
+//
+//            self.parse_nodes(&next_node);
+//        }
+//    }
+//
+//    fn next_nodes(&mut self, cur_node: &Node) -> Vec<Node> {
+//        let mut nexts: Vec<Node> = Vec::new();
+//        
+//        for p in [[0,1],[0,-1],[1,0],[-1,0]].iter() {
+//            if (cur_node.pos.x == 0 && p[0] < 0) || (cur_node.pos.y == 0 && p[1] < 0) {
+//                continue;
+//            }
+//
+//            let np = Pos {
+//                x: ((cur_node.pos.x as i32) + p[0]) as usize,
+//                y: ((cur_node.pos.y as i32) + p[1]) as usize,
+//            };
+//
+//            if self.is_valid(&np, &cur_node) {
+//                let next_node = self.nodes.entry(np).or_insert(Node {pos: np, step: -1, prev: None});
+////                let b = Box::new(Node {pos: np, step: -1, prev: None});
+////                let next_node = self.nodes.entry(np).or_insert(b);
+//                nexts.push(next_node.clone());
+//            }
+//        }
+//        
+//        nexts
+//    }
+
+    fn is_valid(&self, pos: &Pos, cur_node: &Node) -> bool {
+        match self.hmap.get(pos) {
+            Some(v) => {
+                if v == "1" {
+                    return false;
+                }
+            },
+            None => {
+                return false;
+            }
+        }
+
+        if let Some(p) = &cur_node.prev {
+            if p.pos.x == pos.x && p.pos.y == pos.y {
+                return false;
+            }
+        }
+
+        true
+    }
 }
 
 fn main() {
-   let (wh, rmap) = init();
-   let goals = find_routes(wh, rmap);
-   let min = min_steps(&goals);
-
-   if min == 0 {
-    println!("Fail");
-    return;
-   }
-
-   println!("{}", min);
+//    let s = ""; //@@@
+//
+//    let mut gmap = GameMap::new(s);
+//    //println!("{:?}", gmap);
+//
+//    let goal = gmap.goal_node();
+//    //println!("{:?}", goal);
+//    //print_route(&goal);
+//    
+//    match goal {
+//        Some(node) => {
+//            println!("{}", node.step);
+//        },
+//        None => println!("Fail")
+//    }
 }
 
-fn read<T: std::str::FromStr>() -> T {
-  let mut s = String::new();
-  std::io::stdin().read_line(&mut s).ok();
-  s.trim().parse().ok().unwrap()
-}
-
-fn read_vec<T: std::str::FromStr>() -> Vec<T> {
-  read::<String>().split_whitespace()
-      .map(|e| e.parse().ok().unwrap()).collect()
-}
-
-fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
-  (0..n).map(|_| read_vec()).collect()
-}
+//fn read<T: std::str::FromStr>() -> T {
+//    let mut s = String::new();
+//    std::io::stdin().read_line(&mut s).ok();
+//    s.trim().parse().ok().unwrap()
+//}
+//
+//fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+//    read::<String>().split_whitespace()
+//        .map(|e| e.parse().ok().unwrap()).collect()
+//}
+//
+//fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+//    (0..n).map(|_| read_vec()).collect()
+//}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn make(str: &str) -> (Vec<String>, Vec<Vec<String>>) {
-      let mut lines = str.lines();
-
-      let wh = lines.next().unwrap().split_whitespace()
-        .map(|e| e.parse().ok().unwrap()).collect();
-
-      let rmap = lines.map(|l| l.split_whitespace()
-          .map(|e| e.parse().ok().unwrap()).collect()).collect();
-
-      (wh, rmap)
-    }
-
     #[test]
     fn test_find_routes() {
-      let inp = "\
+        let s = "\
 4 5
 0 s 0 1
 0 0 1 0
@@ -234,14 +231,14 @@ mod tests {
 0 0 1 g
 0 0 0 0";
 
-      let (wh, rmap) = make(inp);
-      println!("{:?}", wh);
-      println!("{:?}", rmap);
-      let goals = find_routes(wh, rmap);
+        let mut gmap = GameMap::new(s);
+        //println!("{:?}", gmap);
 
-      println!("{:#?}", goals);
-      print_route_all(&goals);
-
-      assert_eq!(min_steps(&goals), 9);
+//        let goal = gmap.goal_node();
+//        //println!("{:?}", goal);
+//        //print_route(&goal);
+//
+//        assert_eq!(goal.pos, Pos {x: 3, y: 3});
+//        assert_eq!(goal.step, 9);
     }
 }
